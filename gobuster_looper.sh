37a1130e61e5e66ceee8f950566d67cc1dcbaf68
Fabian <fabiananguiano@gmail.com>
2021-05-22 00:11:22 -0500

    Update gobuster_looper.sh

--- gobuster_looper.sh
+++ gobuster_looper.sh
@@ -2,9 +2,8 @@
 echo Feed me a textfile
 read file
 echo "Your file is $file"
-
+echo point me to a wordlist
+read wordlist
 while read -r line; do echo "Busting $line"; done < $file
-while read -r line; do gobuster dir --wildcard -u $line -w /root/wfuzz/wordlist/general/common.txt -e -k -s 200; done < $file > gobuster_results.txt
-echo "Done, results saved to gobuster_results.txt "
-~
-
+while read -r line; do gobuster dir --wildcard -u $line -w $wordlist -e -k; done < $file > gobuster_results.txt
+echo "Done, results saved to gobuster_results.txt"
