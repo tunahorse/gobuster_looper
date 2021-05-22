63ba19c4a9eb836e56dc1ce6c6de33dac9553d57
Fabian Anguiano <fabiananguiano@gmail.com>
2021-05-22 00:15:14 -0500

    fixed wordlist input

--- gobuster_looper.sh
+++ gobuster_looper.sh
@@ -6,4 +6,4 @@ echo point me to a wordlist
 read wordlist
 while read -r line; do echo "Busting $line"; done < $file
 while read -r line; do gobuster dir --wildcard -u $line -w $wordlist -e -k; done < $file > gobuster_results.txt
-echo "Done, results saved to gobuster_results.txt"
+echo "Done, results saved to gobuster_results.txt "
